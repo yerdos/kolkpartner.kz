b945447942981419641f9407cb2b285d8e114306
yerdos <yerdos7@gmail.com>
2026-01-01 20:08:22 +0500

    UPDATE

--- crawler/logging/logger.py
+++
@@ -1,4 +0,0 @@
-from logging.logger import logger
-
-logger.info("start crawl guazi")
-logger.error("insert failed", exc_info=True)
--- crawler/pricing/rounding.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def ceil_to_10(x) -> int:
-    if not x or x <= 0:
-        return 0
-    return int(math.ceil(float(x) / 10) * 10)
